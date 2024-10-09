c991cacb54a8cb3db6874300faabf24ca06a44a1
Liador <lousbergje@gmail.com>
2024-10-09 09:09:16 +0200

    improved commection buttons

--- Assets/Scripts/EaseOfUseScript.cs
+++ Assets/Scripts/EaseOfUseScript.cs
@@ -5,10 +5,20 @@ public class EaseOfUseScript : MonoBehaviour
 {
     void OnGUI()
     {
-        if (GUI.Button(new Rect(50, 50, 150, 50), "StartHost"))
-            NetworkManager.Singleton.StartHost();
-            
-        if (GUI.Button(new Rect(50, 150, 150, 50), "StartClient"))
-            NetworkManager.Singleton.StartClient();
+        if (NetworkManager.Singleton.IsServer || NetworkManager.Singleton.IsClient)
+        {
+            if (GUI.Button(new Rect(50, 750, 150, 50), "Disconnect"))
+                NetworkManager.Singleton.Shutdown();
+        }
+        else
+        {
+            if (GUI.Button(new Rect(50, 50, 150, 50), "StartHost"))
+                NetworkManager.Singleton.StartHost();
+
+            if (GUI.Button(new Rect(50, 150, 150, 50), "StartClient"))
+                NetworkManager.Singleton.StartClient();
+            if (GUI.Button(new Rect(50, 250, 150, 50), "StartServer"))
+                NetworkManager.Singleton.StartServer();
+        }
     }
 }
