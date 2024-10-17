61f74ab6373e329a42722507826ee7cbcb78ca23
Liador <lousbergje@gmail.com>
2024-10-17 11:03:34 +0200

    Added IP address to connect as a client and displayed IP when connected

--- Assets/Scripts/EaseOfUseScript.cs
+++ Assets/Scripts/EaseOfUseScript.cs
@@ -1,24 +1,58 @@
+using System;
+using System.Net;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 using UnityEngine;
 
 public class EaseOfUseScript : MonoBehaviour
 {
+    private UnityTransport mUnityTransport;
+    private string mIpAdress;
+
+    void Awake()
+    {
+        mUnityTransport = GetComponent<UnityTransport>();
+        mIpAdress = "Enter IP";
+    }
+
     void OnGUI()
     {
+        var style = new GUIStyle();
+        style.fontSize = 30;
         if (NetworkManager.Singleton.IsServer || NetworkManager.Singleton.IsClient)
         {
-            if (GUI.Button(new Rect(50, 750, 150, 50), "Disconnect"))
-                NetworkManager.Singleton.Shutdown();
+            GUI.enabled = false;
+            var text = GUI.TextField(
+                new Rect(0, 0, 150, 0),
+                mUnityTransport.ConnectionData.Address,
+                style
+            );
+            GUI.enabled = true;
+            if (NetworkManager.Singleton.IsServer)
+                if (GUI.Button(new Rect(50, 750, 150, 50), "Disconnect"))
+                    NetworkManager.Singleton.Shutdown();
         }
         else
         {
-            if (GUI.Button(new Rect(50, 50, 150, 50), "StartHost"))
-                NetworkManager.Singleton.StartHost();
-
+            mIpAdress = GUI.TextField(new Rect(0, 0, 150, 30), mIpAdress, 16);
+            var isIpValid = IsValidIP(mIpAdress);
+            if (isIpValid)
+            {
+                mUnityTransport.SetConnectionData(mIpAdress, 7777);
+            }
+            GUI.enabled = isIpValid;
             if (GUI.Button(new Rect(50, 150, 150, 50), "StartClient"))
                 NetworkManager.Singleton.StartClient();
+            GUI.enabled = true;
+            if (GUI.Button(new Rect(50, 50, 150, 50), "StartHost"))
+                NetworkManager.Singleton.StartHost();
             if (GUI.Button(new Rect(50, 250, 150, 50), "StartServer"))
                 NetworkManager.Singleton.StartServer();
         }
+    }
+
+    private bool IsValidIP(string text)
+    {
+        return IPAddress.TryParse(text, out IPAddress ip);
     }
 }
